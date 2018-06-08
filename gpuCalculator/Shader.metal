1dd9cd6d5a6d2a8fdd6eb1f8b85bdd6ca4598150
Yasaman Marcu <ymarcu@connectedlab.com>
2018-06-08 10:03:54 -0400

    Clean up for demo

--- gpuCalculator/Shader.metal
+++ gpuCalculator/Shader.metal
@@ -9,16 +9,10 @@
 #include <metal_stdlib>
 using namespace metal;
 
-// GPU function to calculate
-kernel void kernal_function(const device float *inVector [[ buffer(0) ]],
-                    device float *outVector [[ buffer(1) ]],
-                    uint id [[ thread_position_in_grid ]])
-{
-    outVector[id] = 1.0 / (1.0 + exp(-inVector[id]));
-}
-
-kernel void shader(device int &printBuffer [[buffer(0)]],
+kernel void shader(device int *printBuffer [[ buffer(0) ]],
                    uint id [[ thread_position_in_grid ]]) {
     
-    printBuffer = 98123;
+    printBuffer[id] = 98123;
 }
+
+
--- gpuCalculator/ViewController.swift
+++ gpuCalculator/ViewController.swift
@@ -10,116 +10,53 @@ import UIKit
 import Metal
 
 class ViewController: UIViewController {
-    var device: MTLDevice!
-    
-    // optional
-    var renderPassDescriptor: MTLRenderPassDescriptor!
-    var renderPipelineState: MTLRenderPipelineState!
-    
-    // Mandatory
-    var threadsPerGroup:MTLSize!
-    var numberOfThreadgroups:MTLSize!
-    
-    var defaultLibrary: MTLLibrary!
-    var inBuffer: MTLBuffer!
-    var outBuffer: MTLBuffer!
-    var computePipelineState: MTLComputePipelineState!
-    
-    var commandQueue: MTLCommandQueue!
-    var commandBuffer: MTLCommandBuffer!
-    var computeCommandEncoder: MTLComputeCommandEncoder! //converts into byte code for buffer
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        device = MTLCreateSystemDefaultDevice()
-        defaultLibrary = device.makeDefaultLibrary()
-        commandQueue = device.makeCommandQueue()
-        commandBuffer = commandQueue.makeCommandBuffer()
-        computeCommandEncoder = commandBuffer.makeComputeCommandEncoder()
-        singleDimentionCalculation(kernalFunctionName: "shader")
-        
-        
-//       doubleDimentionCalculation()
-    }
     
+        // GPU interface
+        let device: MTLDevice! = MTLCreateSystemDefaultDevice()
     
-    func doubleDimentionCalculation () {
-        device = MTLCreateSystemDefaultDevice()
+        // shader function
+        let defaultLibrary: MTLLibrary! = device.makeDefaultLibrary()
+        let kernalFunction = defaultLibrary.makeFunction(name: "shader")!
+        let computePipelineState: MTLComputePipelineState! = try! device.makeComputePipelineState(function: kernalFunction) // holds the compiled shader code
         
-        // buffer
-        let count = 1500
-        let length = count * MemoryLayout< Float >.stride
-        var myVector = [Float](repeating: 0, count: count)
-        outBuffer = device.makeBuffer(bytes: myVector, length: length, options: [])
-        for (index, _) in myVector.enumerated() { myVector[index] = Float(index) }
-        inBuffer = device.makeBuffer(bytes: myVector, length: length, options: [])
+        let commandQueue: MTLCommandQueue! = device.makeCommandQueue() // queue up a list of commands
+        let commandBuffer: MTLCommandBuffer! = commandQueue.makeCommandBuffer() // stores the encoded commands
+        let computeCommandEncoder: MTLComputeCommandEncoder! = commandBuffer.makeComputeCommandEncoder() // encodes to byte code
         
-        defaultLibrary = device.makeDefaultLibrary()!
-        let kernalFunction = defaultLibrary.makeFunction(name: "kernal_function")
-        computePipelineState = try! device.makeComputePipelineState(function: kernalFunction!)
-        
-        // thread groups
-        let size = MTLSize(width: count, height: 1, depth: 1)
-        //        let w = computePipelineState.threadExecutionWidth
-        //        let h = computePipelineState.maxTotalThreadsPerThreadgroup / w
-        //        threadsPerGroup = MTLSizeMake(w, h, 1)
-        //        numberOfThreadgroups = MTLSize(width: 1024/w,
-        //                                       height: 1024/h,
-        //                                       depth: 1)
-        
-        
-        
-        commandQueue = device.makeCommandQueue()
-        commandBuffer = commandQueue.makeCommandBuffer()
-        computeCommandEncoder = commandBuffer.makeComputeCommandEncoder()
+        let resultdata: [Int] = [0]
+        let buffer: MTLBuffer! = device.makeBuffer(bytes: resultdata, length: MemoryLayout<Int>.stride, options: [])    // 8 bits
+    
         computeCommandEncoder.setComputePipelineState(computePipelineState)
-        computeCommandEncoder.setBuffer(inBuffer, offset: 0, index: 0)
-        computeCommandEncoder.setBuffer(outBuffer, offset: 0, index: 1)
-        computeCommandEncoder.dispatchThreadgroups(size, threadsPerThreadgroup: size)
+        computeCommandEncoder.setBuffer(buffer, offset: 0, index: 0)
+
+//        let w = computePipelineState.threadExecutionWidth   // 32
+//        let h = computePipelineState.maxTotalThreadsPerThreadgroup/w // 1024/32
+//        let threadgroupsPerGrid = MTLSizeMake(w, h, 1)
+//        let threadsPerThreadgroup = MTLSize(width: 1024/w,height: 1024/h,depth: 1)
+        
+        let threadgroupsPerGrid = MTLSize(width:1, height:1, depth:1)
+        let threadsPerThreadgroup = MTLSize(width:1,height:1,depth:1)
+        computeCommandEncoder.dispatchThreadgroups(threadgroupsPerGrid, threadsPerThreadgroup: threadsPerThreadgroup)
         computeCommandEncoder.endEncoding()
+        
+        commandBuffer.addCompletedHandler {commandBuffer in
+            let executionDuration = commandBuffer.gpuEndTime - commandBuffer.gpuStartTime
+            print("time: \(executionDuration) seconds")
+            let data = NSData(bytes: buffer.contents(), length: MemoryLayout<NSInteger>.stride)
+            var out: NSInteger = 0
+            data.getBytes(&out, length: MemoryLayout<NSInteger>.stride)
+            print("data: \(out)")
+        }
+        
         commandBuffer.commit()
-        
-        let result = outBuffer.contents().bindMemory(to: Float.self, capacity: count)
-        var data = [Float](repeating:0, count: count)
-        for i in 0 ..< count { data[i] = result[i] }
-        print(data.map { $0 })
-        
-    }
-    
-    func singleDimentionCalculation (kernalFunctionName: String) {
-        do
-        {
-            computePipelineState = try device.makeComputePipelineState(function: defaultLibrary.makeFunction(name: kernalFunctionName)!)
-            computeCommandEncoder.setComputePipelineState(computePipelineState)
-            let resultdata = [Int](repeating: 0, count: 1)
-            outBuffer = device.makeBuffer(bytes: resultdata, length: 1 * MemoryLayout<Int>.stride, options: [])
-            computeCommandEncoder.setBuffer(outBuffer, offset: 0, index: 0)
-            
-            let threadsPerGroup = MTLSize(width:1,height:1,depth:1)
-            let numThreadgroups = MTLSize(width:1, height:1, depth:1)
-            computeCommandEncoder?.dispatchThreadgroups(numThreadgroups, threadsPerThreadgroup: threadsPerGroup)
-            computeCommandEncoder?.endEncoding()
-            
-            commandBuffer?.addCompletedHandler {commandBuffer in
-                let data = NSData(bytes: self.outBuffer.contents(), length: MemoryLayout<NSInteger>.stride)
-                var out: NSInteger = 0
-                data.getBytes(&out, length: MemoryLayout<NSInteger>.stride)
-                print("data: \(out)")
-            }
-            
-            commandBuffer?.commit()
-            
-        }
-        catch
-        {
-            fatalError("newComputePipelineStateWithFunction failed ")
-        }
+
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
     
 }
